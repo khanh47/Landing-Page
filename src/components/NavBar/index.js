2b5559f392422321544e5f1e0cd488a0d1062658
khanh47 <phhkhanh47@gmail.com>
2022-09-23 21:33:47 +0700

    add new files

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,27 @@
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div class="header">
+      <div class="header_navbar">
+        <div class="home">
+          <h1 class="home1">CUỘC CHIẾN</h1>
+          <div>
+            <h1 class="home2">VƯƠNG QUYỀN</h1>
+            <img class="logoCCVQ" src="logoCCVQ.png" alt="logoCCVQ" />
+          </div>
+          <img class="searchIcon" src="searchIcon.png" alt="searchIcon" />
+        </div>
+        <ul class="header_navbar_list">
+          <li class="list">Giới thiệu</li>
+          <li class="list">Ban tổ chức</li>
+          <li class="list">Cuộc thi</li>
+          <li class="list">Liên hệ</li>
+          <li class="list">Hỗ trợ</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
