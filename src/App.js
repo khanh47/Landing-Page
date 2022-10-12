4e4745d1213ea6e42885c030834670e03e002009
Bap <lemaithykhoa@gmail.com>
2022-10-12 14:14:34 +0700

    add images to carousel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
-import About from "./components/About"
+import About from "./components/About";
+import Slide from "./components/Slide"
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <About />
-      <Footer />
+      <Slide />
     </div>
   );
 }
---
+++ src/components/Slide/index.js
@@ -0,0 +1,52 @@
+import "./style.css";
+import CCVQ2021_1 from "./images/CCVQ2021_1.jpg";
+import CCVQ2021_2 from "./images/CCVQ2021_2.jpg";
+import CCVQ2021_3 from "./images/CCVQ2021_3.jpg";
+import CCVQ2021_4 from "./images/CCVQ2021_4.jpg";
+import CCVQ2021_5 from "./images/CCVQ2021_5.jpg";
+import CCVQ2021_6 from "./images/CCVQ2021_6.jpg";
+import CCVQ2021_7 from "./images/CCVQ2021_7.jpg";
+import CCVQ2021_8 from "./images/CCVQ2021_8.jpg";
+import CCVQ2021_9 from "./images/CCVQ2021_9.jpg";
+import CCVQ2021_10 from "./images/CCVQ2021_10.jpg";
+import CCVQ2022_1 from "./images/CCVQ2022_1.jpg";
+import CCVQ2022_2 from "./images/CCVQ2022_2.jpg";
+import CCVQ2022_3 from "./images/CCVQ2022_3.jpg";
+import CCVQ2022_4 from "./images/CCVQ2022_4.jpg";
+import CCVQ2022_5 from "./images/CCVQ2022_5.jpg";
+import CCVQ2022_6 from "./images/CCVQ2022_6.jpg";
+import CCVQ2022_7 from "./images/CCVQ2022_7.jpg";
+import CCVQ2022_8 from "./images/CCVQ2022_8.jpg";
+import CCVQ2022_9 from "./images/CCVQ2022_9.jpg";
+import CCVQ2022_10 from "./images/CCVQ2022_10.jpg";
+
+function About_us() {
+  return (
+    <body>
+      <div className="list_images">
+        <div><img src={CCVQ2021_1} alt=""></img></div>
+        <div><img src={CCVQ2021_2} alt=""></img></div>
+        <div><img src={CCVQ2021_3} alt=""></img></div>
+        <div><img src={CCVQ2021_4} alt=""></img></div>
+        <div><img src={CCVQ2021_5} alt=""></img></div>
+        <div><img src={CCVQ2021_6} alt=""></img></div>
+        <div><img src={CCVQ2021_7} alt=""></img></div>
+        <div><img src={CCVQ2021_8} alt=""></img></div>
+        <div><img src={CCVQ2021_9} alt=""></img></div>
+        <div><img src={CCVQ2021_10} alt=""></img></div>
+        <div><img src={CCVQ2022_1} alt=""></img></div>
+        <div><img src={CCVQ2022_2} alt=""></img></div>
+        <div><img src={CCVQ2022_3} alt=""></img></div>
+        <div><img src={CCVQ2022_4} alt=""></img></div>
+        <div><img src={CCVQ2022_5} alt=""></img></div>
+        <div><img src={CCVQ2022_6} alt=""></img></div>
+        <div><img src={CCVQ2022_7} alt=""></img></div>
+        <div><img src={CCVQ2022_8} alt=""></img></div>
+        <div><img src={CCVQ2022_9} alt=""></img></div>
+        <div><img src={CCVQ2022_10} alt=""></img></div>
+      </div>
+    </body>
+  );
+}
+
+export default About_us;
