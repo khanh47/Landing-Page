3c55d40ade7e930407852ad1b49d78aece274b39
duckie2709 <nltd.27092004@gmail.com>
2023-01-14 21:24:38 +0700

    htmltojs

---
+++ src/components/About/CCVQ17/CCVQ17.js
@@ -0,0 +1,30 @@
+import 'style.css';
+import 'arr.png';
+import 'arl.png';
+import 'img21.jpg';
+import 'img22.jpg';
+import 'img23.jpg';
+import 'img31.jpg';
+import 'img32.jpg';
+const CCVQ17 = () => {
+    return (
+        <body>
+            <div class="ccvq2018">
+                <div class="div1">CCVQ 2017</div>
+                <div class="div2">
+                    <div class="div21"><img class="img2" src="img21.jpg"></div>
+                    <div class="div22"><img class="img2" src="img22.jpg"></div>
+                    <div class="div23"><img class="img2" src="img23.jpg"></div>
+                </div>
+                <div class="div3">
+                    <div class="div31"><img class="img31" src="img31.jpg"></div>
+                    <div class="div32"><img class="img32" src="img32.jpg"></div>
+                </div>
+                <div class="div4">
+                    <div class="div41"><img src="arl.png"></div>
+                    <div class="div42"><img src="arr.png"></div>
+                </div>
+        </body>
+    );
+};
+    
